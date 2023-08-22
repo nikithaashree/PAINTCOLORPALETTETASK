0220e2bfe762a7a2efab179e5d49cf9cee8efa90
nikithaashree <nikithaa292507@gmail.com>
2023-08-22 17:25:11 +0530

    Color Palette task
    This WPF application is about color palette.

---
+++ PAINT_COLOUR_PALETTE_TASK_31.07.23/PAINT_COLOUR_PALETTE_TASK_31.07.23/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PAINT_COLOUR_PALETTE_TASK_31._07._23
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ PAINT_COLOUR_PALETTE_TASK_31.07.23/PAINT_COLOUR_PALETTE_TASK_31.07.23/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿
+using System.Windows;
+using System.Windows.Input;
+
+
+
+namespace PAINT_COLOUR_PALETTE_TASK_31._07._23
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void lblcrimson_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            lblmain.Background = lblcrimson.Background; 
+        }
+
+        private void lblkhaki_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+lblmain.Background = lblkhaki.Background;
+        }
+
+        private void lbldarkgoldenrod_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+lblmain.Background=lbldarkgoldenrod.Background;
+        }
+
+        private void lbldarkseagreen_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+lblmain.Background=lbldarkseagreen.Background;
+        }
+
+        private void lblbisque_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+lblmain.Background= lblbisque.Background;
+        }
+
+        private void lblaquamarine_MouseEnter(object sender, MouseEventArgs e)
+        {
+lblmain.Background= lblaquamarine.Background;
+        }
+
+        private void lbldarkgreen_MouseEnter(object sender, MouseEventArgs e)
+        {
+            lblmain.Background = lbldarkgreen.Background;
+        }
+
+        private void lbldarkred_MouseEnter(object sender, MouseEventArgs e)
+        {
+            lblmain.Background = lbldarkred.Background;
+        }
+
+        private void lblburlywood_MouseLeave(object sender, MouseEventArgs e)
+        {
+            lblmain.Background = lblburlywood.Background;
+        }
+
+        private void lbldarkviolet_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            lblmain.Background=lbldarkviolet.Background;
+        }
+
+        private void lbldarkslateblue_MouseMove(object sender, MouseEventArgs e)
+        {
+lblmain.Background = lbldarkslateblue.Background;
+        }
+
+        private void lbllemonchiffon_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            lblmain.Background= lbllemonchiffon.Background;
+        }
+    }
+
+}
